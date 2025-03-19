f63a4973444482e5347c45bc15b7fd5d9373fb19
mclara9593 <mclara9593@gmail.com>
2025-03-19 09:21:16 -0300

    first_event_FAQ_model

--- UEFSevents/FAQ/models.py
+++ UEFSevents/FAQ/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+#lembrar de colocar Foreignkey de usuário e eventos
+
+class questions(models.Model):
+    question_description = models.CharField(max_length = 1000)
+    question_likes = models.IntegerField()
+    question_dislike = models.IntegerField()
+
+class answers(models.Model):
+    answer_description = models.CharField(max_length = 1000)
+    answer_fk_question = models.ForeignKey('questions', on_delete = models.CASCADE)
+
+class complaints(models.Model):
+    complaint_status = models.CharField(max_length = 100)
+    complaint_reason = models.CharField(max_length = 500)
