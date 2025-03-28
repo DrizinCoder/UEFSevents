4d13dea40cf31fc18d581edf531f89027d817de8
cauan reis <cauan.reis30099@gmail.com>
2025-03-27 22:09:15 -0300

    primeira view da FAQ

--- UEFSevents/FAQ/models.py
+++ UEFSevents/FAQ/models.py
@@ -2,22 +2,20 @@ from django.db import models
 from Users.models import CustomUser
 from Events.models import Event
 
-#lembrar de colocar Foreignkey de usuário e eventos
 
-class questions(models.Model):
+class Questions(models.Model):
     question_description = models.CharField(max_length = 1000)
     question_likes = models.IntegerField()
     question_dislike = models.IntegerField()
     question_fk_user = models.ForeignKey(CustomUser, on_delete = models.CASCADE, null=True)
     question_fk_events = models.ForeignKey(Event, on_delete = models.CASCADE, null=True)
 
-class answers(models.Model):
+class Answers(models.Model):
     answer_description = models.CharField(max_length = 1000)
-    answer_fk_question = models.ForeignKey(questions, on_delete = models.CASCADE, null=True)
+    answer_fk_question = models.ForeignKey(Questions, on_delete = models.CASCADE, null=True)
     answers_fk_users = models.ForeignKey(CustomUser, on_delete = models.CASCADE, null=True)
 
-
-class complaints(models.Model):
+class Complaints(models.Model):
     complaint_status = models.CharField(max_length = 100)
     complaint_reason = models.CharField(max_length = 500)
     complaint_fk_user = models.ForeignKey(CustomUser, on_delete = models.CASCADE, null=True)
--- UEFSevents/FAQ/views.py
+++ UEFSevents/FAQ/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import ListView
+from .models import Questions, Answers
 
-# Create your views here.
+class ViewRead(ListView):
+    model = Questions
+    template_name = 'Lista_de_Perguntas.html'
+    context_object_name = 'Perguntas'
