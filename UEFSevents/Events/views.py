b9b969951841c9e89eea39e3fa4764347ea965c5
th3n00bc0der22 <dogpolar22@gmail.com>
2025-04-08 21:10:07 -0300

    autenticacao de events em tres operacoes do crud

--- UEFSevents/Events/views.py
+++ UEFSevents/Events/views.py
@@ -5,7 +5,20 @@ from .serializers import EventSerializer, AdressSerializer, ImageSerializer, Spa
 class EventViewSet(viewsets.ModelViewSet):
     queryset=Event.objects.all()
     serializer_class=EventSerializer
+from rest_framework import viewsets, permissions
 
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+    # Método para definir permissões dinamicamente
+    def get_permissions(self):
+        # Ações que requerem autenticação
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [permissions.IsAuthenticated()]
+        # Ações públicas (list, retrieve)
+        return []
+    
 class AdressViewSet(viewsets.ModelViewSet):
     queryset=Adress.objects.all()
     serializer_class=AdressSerializer
