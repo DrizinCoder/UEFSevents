2c1a0805e250270518bfadb76dc0fae3adf569c5
mclara9593 <mclara9593@gmail.com>
2025-04-15 20:05:42 -0300

    Event"s filters

--- UEFSevents/Events/views.py
+++ UEFSevents/Events/views.py
@@ -1,32 +1,35 @@
 from rest_framework import viewsets
 from .models import Event, Adress, Space, Image
 from .serializers import EventSerializer, AdressSerializer, ImageSerializer, SpaceSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 
-class EventViewSet(viewsets.ModelViewSet):
-    queryset=Event.objects.all()
-    serializer_class=EventSerializer
-from rest_framework import viewsets, permissions
 
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter] 
+    filterset_fields = ['categoria', 'tipo', 'data', 'limite_de_idade', 'adress'] 
+    search_fields = ['categoria', 'tipo', 'data', 'limite_de_idade'] 
+    # ordering = ['']
+    # ordering_fields = ['']
 
-    # Método para definir permissões dinamicamente
-    def get_permissions(self):
-        # Ações que requerem autenticação
-        if self.action in ['create', 'update', 'partial_update', 'destroy']:
-            return [permissions.IsAuthenticated()]
-        # Ações públicas (list, retrieve)
-        return []
-    
 class AdressViewSet(viewsets.ModelViewSet):
     queryset=Adress.objects.all()
     serializer_class=AdressSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['cidade', 'estado','']
+    search_fields = ['cidade', 'estado','']
 
 class SpaceViewSet(viewsets.ModelViewSet):
     queryset=Space.objects.all()
     serializer_class=SpaceSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['acessibilidade']
+    search_fields = ['acessibilidade']
 
 class ImageViewSet(viewsets.ModelViewSet):
     queryset=Image.objects.all()
     serializer_class=ImageSerializer
+    filterset_fields = ['titulo']
+    search_fields = ['titulo']
