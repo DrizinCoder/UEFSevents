bc6c5ba3c16473f04f47c85b1cf9ff493fc9ce82
cauan reis <cauan.reis30099@gmail.com>
2025-04-29 21:15:35 -0300

    correção FAQ

--- UEFSevents/FAQ/tests/integrated_test.py
+++ UEFSevents/FAQ/tests/integrated_test.py
@@ -1,1 +1,41 @@
+from django.test import TestCase
+from django.test.client import Client
+from django.urls import reverse
+from time import timezone
 from ..models import Questions
+from Events.models import Event, Space
+
+
+class FAQBaseEventTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+        self.space = Space.objects.create(
+            max_capacity = 30,
+            name = 'Rua do palhaço',
+            acessibility = True,
+            phone = '75999999999',
+            mobile = '7588888888',
+            type_adress = 'avenida'
+        )
+
+        self.event = Event.objects.create(
+            title = 'Gang Bang',
+            description = 'Preciso dizer?',
+            start_date = timezone.now(),
+            end_date = timezone.now(),
+            start_time = timezone.now().time(),
+            endtime = timezone.now().time(),
+            status = True,
+            category = 'Adulto',
+            space = self.space,
+            type_event = 'Adulto',
+            age_range = 18
+        )
+
+class QuestionIntegratedTestCase(FAQBaseEventTestCase):
+    def setUp(self):
+        FAQBaseEventTestCase.setUp(self)
+
+    def test_event_create_by_POST(self):
+        url = reverse('criar_evento')
--- UEFSevents/FAQ/tests/unit_test.py
+++ UEFSevents/FAQ/tests/unit_test.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.utils import timezone
-from FAQ.models import Questions, Answers, Complaints
+from ..models import Questions, Answers, Complaints
 from Events.models import Event, Space
 from Users.models import CustomUser
 
@@ -8,33 +8,33 @@ from Users.models import CustomUser
 class FAQBaseEventTestCase(TestCase):
     def setUp(self):
         self.user = CustomUser.objects.create_user(
-            username = 'Clovis_Basilio_dos_Santos',
+            username = 'Ninja_Gamer',
             vat = '12345678901',
             phone = '75999999999',
             mobile = '7588888888',
-            password = 'k8424n71l1'
+            password = 'regra5'
         )
 
         self.space = Space.objects.create(
             max_capacity = 30,
-            name = 'Rua do cacete',
+            name = 'Aldeia Ninja',
             acessibility = True,
             phone = '75999999999',
             mobile = '7588888888',
-            type_adress = 'avenida'
+            type_adress = 'aldeia'
         )
 
         self.event = Event.objects.create(
-            title = 'Gang Bang',
-            description = 'Preciso dizer?',
+            title = 'Cantar 7Minutoz',
+            description = 'Rap do Coringa',
             start_date = timezone.now(),
             end_date = timezone.now(),
             start_time = timezone.now().time(),
             endtime = timezone.now().time(),
             status = True,
-            category = 'Adulto',
+            category = 'Todas as idades',
             space = self.space,
-            type_event = 'Adulto',
+            type_event = 'Todas as idades',
             age_range = 18
         )
 
@@ -43,7 +43,7 @@ class QuestionsTestCase(FAQBaseEventTestCase, TestCase):
         FAQBaseEventTestCase.setUp(self)
 
         self.question = Questions.objects.create(
-            question_description = 'Quem é o dono do evento?',
+            question_description = 'Sabe oq acontece quando vc encontra um doente mental tratado com lixo pela sociedade?',
             question_likes = 51,
             question_dislikes = 51,
             question_fk_user = self.user,
@@ -52,7 +52,7 @@ class QuestionsTestCase(FAQBaseEventTestCase, TestCase):
         )
     
     def test_sucess_create_question(self):
-        self.assertEqual(self.question.question_description, 'Quem é o dono do evento?')
+        self.assertEqual(self.question.question_description, 'Sabe oq acontece quando vc encontra um doente mental tratado com lixo pela sociedade?')
         self.assertNotEqual(self.question.question_description, '')
 
     def test_likes(self):
@@ -66,13 +66,13 @@ class QuestionsTestCase(FAQBaseEventTestCase, TestCase):
 
     def test_relation_question_event(self):
         self.assertEqual(self.question.question_fk_events.id, self.event.id)
-        self.assertEqual(self.question.question_fk_events.title, 'Gang Bang')
+        self.assertEqual(self.question.question_fk_events.title, 'Cantar 7Minutoz')
 
         if hasattr(self.event, 'questions'):
             self.assertIn(self.question, self.event.questions.all())
     
     def test_relation_question_user(self):
-        self.assertEqual(self.question.question_fk_user.username, 'Clovis_Basilio_dos_Santos')
+        self.assertEqual(self.question.question_fk_user.username, 'Ninja_Gamer')
         self.assertEqual(self.question.question_fk_user.id, self.user.id)
         self.assertEqual(self.question.question_fk_user.vat, "12345678901")
 
@@ -84,15 +84,15 @@ class AnswersTestCase(QuestionsTestCase, TestCase):
         QuestionsTestCase.setUp(self)
 
         self.user_answer = CustomUser.objects.create_user(
-            username = 'Dalafi_Maal',
+            username = 'Haruzin',
             vat = '12345678902',
             phone = '75777777777',
             mobile = '75666666666',
-            password = 'm91k81l961'
+            password = 'gachalife'
         )
 
         self.answer = Answers.objects.create(
-            answer_description = 'O apelido tá na senha',
+            answer_description = 'Você ganha o que merece!',
             answer_fk_question = self.question,
             answer_fk_user = self.user_answer,
             answer_created_at = timezone.now()
@@ -100,15 +100,15 @@ class AnswersTestCase(QuestionsTestCase, TestCase):
     
 
     def test_sucess_create_answer(self):
-        self.assertEqual(self.answer.answer_description, 'O apelido tá na senha')
+        self.assertEqual(self.answer.answer_description, 'Você ganha o que merece!')
         self.assertNotEqual(self.answer.answer_description, '')
     
     def test_relation_question_answer(self):
-        self.assertEqual(self.answer.answer_fk_question.question_description, 'Quem é o dono do evento?')
+        self.assertEqual(self.answer.answer_fk_question.question_description, 'Sabe oq acontece quando vc encontra um doente mental tratado com lixo pela sociedade?')
         self.assertEqual(self.answer.answer_fk_question.id, self.question.id)
     
     def test_relation_user_answer(self):
-        self.assertEqual(self.answer.answer_fk_user.username, 'Dalafi_Maal')
+        self.assertEqual(self.answer.answer_fk_user.username, 'Haruzin')
         self.assertNotEqual(self.answer.answer_fk_user, self.question.question_fk_user)
         self.assertNotEqual(self.user_answer.id, self.user.id)
     
@@ -120,16 +120,16 @@ class ComplaintTestCase(FAQBaseEventTestCase, TestCase):
         FAQBaseEventTestCase.setUp(self)
 
         self.user_complaint = CustomUser.objects.create_user(
-            username = 'Elisa_Sanches',
+            username = 'Sanji',
             vat = '12345678902',
             phone = '75777777777',
             mobile = '75666666666',
-            password = '5l9s1s1n385s'
+            password = 'senhoritaNami'
         )
 
         self.complaint = Complaints.objects.create(
             complaint_status = 'Não Respondida',
-            complaint_reason = 'Minha mulher foi participar',
+            complaint_reason = 'Tocaram a música daquele marimo e a minha não',
             complaint_fk_user = self.user_complaint,
             complaint_fk_event = self.event,
             complaint_created_at = timezone.now()
@@ -139,13 +139,13 @@ class ComplaintTestCase(FAQBaseEventTestCase, TestCase):
         self.assertNotEqual(self.complaint.complaint_status, 'Respondida')
     
     def test_reason(self):
-        self.assertEqual(self.complaint.complaint_reason, 'Minha mulher foi participar')
+        self.assertEqual(self.complaint.complaint_reason, 'Tocaram a música daquele marimo e a minha não')
 
     def test_relation_user_complaint(self):
         self.assertEqual(self.complaint.complaint_fk_user.id, self.user_complaint.id)
-        self.assertEqual(self.complaint.complaint_fk_user.username, 'Elisa_Sanches')
+        self.assertEqual(self.complaint.complaint_fk_user.username, 'Sanji')
     
     def test_relation_event_complaint(self):
-        self.assertEqual(self.complaint.complaint_fk_event.title, 'Gang Bang')
+        self.assertEqual(self.complaint.complaint_fk_event.title, 'Cantar 7Minutoz')
         self.assertEqual(self.complaint.complaint_fk_event.id, self.event.id)
-        self.assertEqual(self.complaint.complaint_fk_event.description, 'Preciso dizer?')
+        self.assertEqual(self.complaint.complaint_fk_event.description, 'Rap do Coringa')
