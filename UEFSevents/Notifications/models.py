c4532594b56d46b507ef8daf88517ce949256c93
Guilherme Fernandes <guiaulas2020@gmail.com>
2025-05-08 20:02:46 -0300

    Revert "add notification model"
    This reverts commit 8582cc45edde3cb1e0e9890d0ba6f8293afac88c.

--- UEFSevents/Notifications/models.py
+++ UEFSevents/Notifications/models.py
@@ -1,8 +1,3 @@
 from django.db import models
 
-class Notifications(models.Model):
-    notification_id = models.IntegerField(primary_key=True)
-    notification_message = models.CharField(max_length=250)
-    notification_status = models.CharField(max_length=30)
-    notification_fk_user = models.ForeignKey(notification_id, on_delete=models.CASCADE)
-    
+# Create your models here.
