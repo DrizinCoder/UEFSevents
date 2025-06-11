0da444032c0babe76a5772d269b304d24099885b
Guilherme Fernandes <143087629+DrizinCoder@users.noreply.github.com>
2025-06-11 19:04:30 -0300

    feat: adiciona classe Image ao django admin

--- UEFSevents/Events/admin.py
+++ UEFSevents/Events/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Event, Space, Adress
+from .models import Event, Space, Adress, Image
 
 # Register your models here.
 admin.site.register(Event)
 admin.site.register(Space)
 admin.site.register(Adress)
+admin.site.register(Image)
