504790528345696d8c14a260e46a82b880d2a919
Daniel Lucas <danlucas2002@gmail.com>
2025-07-03 05:20:18 -0300

    Fet: tela de login redireciona para homepage

--- viveri/lib/login_page.dart
+++ viveri/lib/login_page.dart
@@ -6,7 +6,11 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'welcome_page.dart';
 import 'forgot_password_page.dart';
 import 'sign_up_page.dart';
-import 'profile_page.dart';
+import 'home_page.dart';
+import 'location_permission_page.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -49,15 +53,7 @@ class _LoginPageState extends State<LoginPage> {
           final userData = json.decode(userResponse.body);
           await prefs.setString('user_data', json.encode(userData));
           
-          Navigator.pushReplacement(  // Alterado para pushReplacement
-            context,
-            MaterialPageRoute(
-              builder: (context) => ProfilePage(
-                userData: userData,
-                accessToken: tokens['access'],
-              ),
-            ),
-          );
+          _goToLocationPermissionPage();
         } else {
           showInvalidDataDialog(context, 'Falha ao obter dados do usuário');
         }
@@ -69,6 +65,15 @@ class _LoginPageState extends State<LoginPage> {
     } finally {
       setState(() => _isLoading = false);
     }
+  }
+
+  Future<void> _goToLocationPermissionPage() async {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => LocationPermissionPage(),
+      ),
+    );
   }
 
   @override
